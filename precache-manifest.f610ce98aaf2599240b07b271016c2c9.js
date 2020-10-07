bad288659f386b2b50946a476e37fbe97ff9781e
jeremiah094 <59361053+jeremiah094@users.noreply.github.com>
2020-10-07 15:26:47 +0100

    Add files via upload

---
+++ precache-manifest.f610ce98aaf2599240b07b271016c2c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f2869db1b6cfbb47a6e06b20fe939d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a07fab95f09e1dda0a8",
+    "url": "/static/css/main.c0ab35ee.chunk.css"
+  },
+  {
+    "revision": "66f20260792136a53a74",
+    "url": "/static/js/2.25b58735.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25b58735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a07fab95f09e1dda0a8",
+    "url": "/static/js/main.0cc6da45.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
